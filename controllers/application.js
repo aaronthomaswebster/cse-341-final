603233ef232c6bd0b77e0ddf2c180a285ac7056c
Aaron Webster <aaron.thomas.webster@gmail.com>
2024-05-22 21:48:47 -0600

    added logic to the application controller

--- controllers/application.js
+++ controllers/application.js
@@ -4,7 +4,14 @@ const model =() => getModel("application");
 
 const getApplicationById = async (req, res) => {
   try {    
-    res.status(200).json({ message: "function getApplicationById" });
+    let application = await model.find({_id: req.params.id}).popluated('jobId').populated('userId').exec().popluate('jobId.ownerId').exec();
+    if(application.length == 0) {
+      return res.status(404).json({ message: "Application not found" });
+    }
+    if(application[0].userId.passport_user_id != req.session.user.id && application[0].jobId.ownerId.passport_user_id != req.session.user.id) {
+      return res.status(401).json({ message: "Unauthorized: you must be the owner of the job or the user who applied to view this application" });
+    }
+    res.status(200).json(application);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
