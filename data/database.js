d193106885a56d987f209a298584e9c48522365a
Braxton Hotton <bhotton25@gmail.com>
2024-05-22 20:17:18 -0600

    Added more validation and removed /login and /logout from swagger

--- data/database.js
+++ data/database.js
@@ -9,27 +9,27 @@ let database;
 let models = {};
 const userSchema = new Schema({
     _id: {type: mongoose.Schema.Types.ObjectId, auto: true}, 
-    name: {type: String, required: true},
-    email: {type: String, required: true},
+    name: {type: String, required: [true, "Name Required"]},
+    email: {type: String, required: [true, "Email Required"], match: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,},
     role: {type: String, enum: ['admin', 'user'], default: 'user'},
     resume: {type: String, default: null}
 });
 const companySchema = new Schema({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    ownerId: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true}
+    name: {type: String, required: [true, "Company Name Required"]},
+    description: {type: String, required: [true, "Company Description Required"]},
+    ownerId: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: [true, "Owner ID Required"]}
 });
 
 const jobSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    salary: {type: Number, required: true},
-    companyId: {type: mongoose.Schema.Types.ObjectId, ref: 'company', required: true}
+    title: {type: String, required: [true, "Job Title Required"]},
+    description: {type: String, required: [true, "Job Description Required"]},
+    salary: {type: Number, required: [true, "Salary Required"]},
+    companyId: {type: mongoose.Schema.Types.ObjectId, ref: 'company', required: [true, "Company ID Required"]}
 });
 
 const applicationSchema = new Schema({
-    jobId: {type: mongoose.Schema.Types.ObjectId, ref: 'job', required: true},
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true},
+    jobId: {type: mongoose.Schema.Types.ObjectId, ref: 'job', required: [true, "Job ID Required"]},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: [true, "User ID Required"]},
     status: {type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending'}
 });
 
