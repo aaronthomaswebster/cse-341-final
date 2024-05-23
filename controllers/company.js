72f9428284e0fbc5133fa8ca608d8a9921f5183f
Braxton Hotton <bhotton25@gmail.com>
2024-05-22 21:52:30 -0600

    Completed company.js controller

--- controllers/company.js
+++ controllers/company.js
@@ -1,10 +1,12 @@
 const { getModel } = require("../data/database");
+const { getUserByPassportId } = require('./user.js');
 
 const model =() => getModel("company");
 
 const getCompanyById = async (req, res) => {
   try {    
-    res.status(200).json({ message: "function getCompanyById" });
+    const company = await model.findById(req.params.id);
+    res.status(200).json(company);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -12,7 +14,8 @@ const getCompanyById = async (req, res) => {
 
 const getAllCompanies = async (req, res) => {
   try {    
-    res.status(200).json({ message: "function getAllCompanies" });
+    const allCompanies = await model.find();
+    res.status(200).json(allCompanies);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -20,15 +23,23 @@ const getAllCompanies = async (req, res) => {
 
 const createCompany = async (req, res) => {
   try {    
-    res.status(200).json({ message: "function createCompany" });
+    const creation = await model.create(req.body);
+    res.status(200).json(creation.populate('ownerId').exec());
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 const updateCompany = async (req, res) => {
-  try {    
-    res.status(200).json({ message: "function updateCompany" });
+  try {
+    const getCompanyOwner = await model.findById(req.params.id)
+    const getPassportID = await getUserByPassportId();
+    if(getPassportID.id == getCompanyOwner.ownerId){
+      const update = await model.findByIdAndUpdate(getCompanyOwner, req.body)
+      res.status(200).json(update);
+    }else{
+      throw Error
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -37,7 +48,14 @@ const updateCompany = async (req, res) => {
 
 const deleteCompany = async (req, res) => {
   try {    
-    res.status(200).json({ message: "function deleteCompany" });
+    const getCompanyOwner = await model.findById(req.params.id)
+    const getPassportID = await getUserByPassportId();
+    if(getPassportID.id == getCompanyOwner.ownerId){
+      const deletion = await model.findByIdAndDelete(getCompanyOwner)
+      res.status(200).json(deletion);
+    }else{
+      throw Error
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
