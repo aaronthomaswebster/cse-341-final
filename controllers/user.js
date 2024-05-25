2597f720464632153076c75032bf20e665e9bc6c
Aaron Webster <aaron.thomas.webster@gmail.com>
2024-05-24 18:01:45 -0600

    updated user Auth and added delete route

--- controllers/user.js
+++ controllers/user.js
@@ -41,7 +41,8 @@ const createUser = async (user) => {
 
 const updateUser = async (req, res) => {
   try {    
-    await model().updateOne({_id: req.params.id}, req.body);
+    let passport_user_id = req.session.user.id;
+    await model().updateOne({passport_user_id: passport_user_id}, req.body);
     res.status(200).json({ message: "User updated" });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -51,7 +52,8 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {    
-    await model().deleteOne({_id: req.params.id});
+    let passport_user_id = req.session.user.id;
+    await model().deleteOne({passport_user_id: passport_user_id});
     res.status(200).json({ message: "User deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/user');
 const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', isAuthenticated, userController.getUser);
-router.put('/:id', isAuthenticated, userController.updateUser);
+router.put('/', isAuthenticated, userController.updateUser);
+router.delete('/', isAuthenticated, userController.deleteUser);
 
 module.exports = router;
