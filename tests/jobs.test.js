3ad827ed6e489a006c819538c501395c7809549d
Aaron Webster <aaron.thomas.webster@gmail.com>
2024-05-30 19:34:18 -0600

    added test stuff

--- tests/jobs.test.js
+++ tests/jobs.test.js
@@ -3,7 +3,11 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 const request = require("supertest");
 const app = require("../server");
 const { initDb, getModel } = require("../data/database");
-let Job;
+let jobModel;
+let userModel;
+let applicationModel;
+let companyModel;
+
 
 let mongoServer;
 
@@ -11,7 +15,11 @@ beforeAll(async () => {
   await initDb(() => {
     console.log("DB Initialized");
   });
-  Job = getModel("job");
+  jobModel = getModel("job");
+  userModel = getModel("user");
+  applicationModel = getModel("application");
+  companyModel = getModel("company");
+
   mongoServer = await MongoMemoryServer.create();
   const uri = mongoServer.getUri();
 
@@ -33,18 +41,44 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
-  await Job.create([
+  const user = await userModel.create([
+    {
+      name: "Developer",
+      description: "Develop stuff",
+      salary: 1234,
+      passport_user_id: "123456",
+    }
+  ]);
+  const company = await companyModel.create([
+    {
+      name: "Developer",
+      description: "Develop stuff",
+      ownerId: user[0]._id,
+    }
+  ]);
+  const job = await jobModel.create([
     {
       title: "Developer",
       description: "Develop stuff",
       salary: 1234,
-      companyId: new mongoose.Types.ObjectId(),
+      companyId: company[0]._id
+    },
+  ]);
+
+  await applicationModel.create([
+    {
+      jobId: job[0]._id,
+      userId: user[0]._id,
+      status: "pending",
     },
   ]);
 });
 
 afterEach(async () => {
-  await Job.deleteMany();
+  await jobModel.deleteMany();
+  await userModel.deleteMany();
+  await applicationModel.deleteMany();
+  await companyModel.deleteMany();
 });
 
 describe("GET /job", () => {
@@ -63,13 +97,39 @@ describe("GET /job", () => {
 
 describe("GET /job/:id", () => {
   it("should return a job by id", async () => {
-    const jobs = await Job.find();
+    const jobs = await jobModel.find();
     const response = await request(app).get(`/job/${jobs[0]._id}`);
     expect(response.status).toBe(200);
     expect(response.body[0]).toMatchObject({
       title: "Developer",
       description: "Develop stuff",
       salary: 1234,
+    });
+  });
+});
+
+
+describe("GET /company", () => {
+  it("should return all companies", async () => {
+    const response = await request(app).get("/company");
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toMatchObject({
+      name: "Developer",
+      description: "Develop stuff",
+    });
+  });
+});
+
+describe("GET /company/:id", () => {
+  it("should return a company by id", async () => {
+    const companies = await companyModel.find();
+    const response = await request(app).get(`/company/${companies[0]._id}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      name: "Developer",
+      description: "Develop stuff",
     });
   });
 });
